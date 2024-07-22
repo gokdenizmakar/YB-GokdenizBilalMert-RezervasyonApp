c289054bceba8dd6be0ba7f6917903f5785dff5a
Mert Nazlı <98025396+mertnzl@users.noreply.github.com>
2024-07-22 13:03:34 +0300

    Booking sınıfı oluşturuldu.

---
+++ YB.Entities/Models/Booking.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.Entities.Models
+{
+    public class Booking:Base
+    {
+        public Room? Room { get; set; }
+        public int RoomNumber { get; set; }
+        public DateOnly CheckinDate { get; set; }
+        public DateOnly CheckoutDate { get; set; }
+        public double TotalPrice { get; set; }
+    }
+}
