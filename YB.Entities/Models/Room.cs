e2e3d79dea17ff8e0aecdfbdfe64a73535b8e7d7
BilalKayhan <fbilalkayhan@gmail.com>
2024-07-22 12:49:52 +0300

    Room ve Room type sınıfları oluşturuldu.

---
+++ YB.Entities/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.Entities.Models
+{
+    public class Room : Base
+    {
+        public Hotel? Hotel { get; set; }
+        public int HotelID { get; set; }
+        public RoomType? RoomType { get; set; }
+        public int TypeID { get; set; }
+        public string? Status { get; set; }
+    }
+}
---
+++ YB.Entities/Models/RoomType.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.Entities.Models
+{
+    public class RoomType :Base
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public double PricePerNight { get; set; }
+        public byte Capacity { get; set; }
+    }
+}
