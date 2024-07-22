16739dbc36b2332fb270792d90ce6b08a02bc564
BilalKayhan <fbilalkayhan@gmail.com>
2024-07-22 13:05:03 +0300

    Payment sınıfı oluşturuldu

---
+++ YB.Entities/Models/Payment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.Entities.Models
+{
+    public class Payment : Base
+    {
+        public Booking? Booking { get; set; }
+        public int BookingID { get; set; }
+        public double Amount { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public string? PaymentMethod { get; set; }
+
+    }
+}
