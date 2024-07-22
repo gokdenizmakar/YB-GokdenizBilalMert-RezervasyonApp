f4395c06e913d3e4189875983b13389cfb9a73dc
gokdenizmakar <154621595+gokdenizmakar@users.noreply.github.com>
2024-07-22 23:38:45 +0300

    İş katmanında interface'ler oluşturuldu.

---
+++ YB.Business/Abstractions/IGenericService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.Business.Abstractions
+{
+    public interface IGenericService<T> where T : IEntity, new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetByID(Guid id);
+        IQueryable<T> GetAll();
+        T Get(Expression<Func<T, bool>> filter);
+        IQueryable<T> GetAllQueryable(Expression<Func<T, bool>> filter);
+        bool IfEntityExists(Expression<Func<T, bool>> filter);
+    }
+}
---
+++ YB.Business/Abstractions/IHotelService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Models;
+
+namespace YB.Business.Abstractions
+{
+    public interface IHotelService : IGenericService<Hotel>
+    {
+    }
+}
