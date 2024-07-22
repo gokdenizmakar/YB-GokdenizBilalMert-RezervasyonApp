e48d2efd8a9fd074251e4d3e96888e91c31c5b64
Mert Nazlı <98025396+mertnzl@users.noreply.github.com>
2024-07-22 21:53:36 +0300

    Dataaccess katmanında interface tanımlandı.

---
+++ YB.DataAccess/Abstractions/IRepositoryDal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.DataAccess.Abstractions
+{
+    public interface IRepositoryDal<T> where T : IEntity,new()
+    {
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        T GetByID(Guid id);
+        IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null);
+        T Get(Expression<Func<T, bool>> filter);
+        IQueryable<T> GetAllQueryable(Expression<Func<T, bool>> filter);
+        bool IfEntityExists(Expression<Func<T, bool>> filter);
+
+    }
+}
--- YB.Entities/Abstraction/Base.cs
+++ YB.Entities/Abstraction/Base.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace YB.Entities.Abstraction
 {
-    public abstract class Base
+    public abstract class Base:IEntity
     {
         protected Base()
         {
