6f6def90d6e8780aedea5e440f31024e4fbc5432
gokdenizmakar <154621595+gokdenizmakar@users.noreply.github.com>
2024-07-22 12:45:03 +0300

    Staff sınıfı oluşturuldu.

---
+++ YB.Entities/Models/Staff.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.Entities.Abstraction;
+
+namespace YB.Entities.Models
+{
+    public class Staff : Base
+    {
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Position { get; set; }
+        public double Salary { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public DateOnly HireDate { get; set; }
+        public Hotel? Hotel { get; set; }
+        public int HotelID { get; set; }
+    }
+}
