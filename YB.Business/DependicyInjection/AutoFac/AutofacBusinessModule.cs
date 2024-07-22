c72d1e8d00c11bf675889b5db028c0fe1e6266c2
BilalKayhan <fbilalkayhan@gmail.com>
2024-07-23 00:18:58 +0300

    Autofac Business module ve Instance factory oluşturuldu.

---
+++ YB.Business/DependicyInjection/AutoFac/AutofacBusinessModule.cs
@@ -0,0 +1,27 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YB.DataAccess.Abstractions;
+using YB.DataAccess.Context;
+using YB.DataAccess.Repositories.EntityFramework;
+
+namespace YB.Business.DependicyInjection.AutoFac
+{
+    public class AutofacBusinessModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ApplicationDbContext>().AsSelf().SingleInstance();
+            builder.RegisterType<EFBookingDal>().As<IBookingDal>();
+            builder.RegisterType<EFGuestDal>().As<IGuestDal>();
+            builder.RegisterType<EFHotelDal>().As<IHotelDal>();
+            builder.RegisterType<EFPaymentDal>().As<IPaymentDal>();
+            builder.RegisterType<EFRoomDal>().As<IRoomDal>();
+            builder.RegisterType<EFRoomTypeDal>().As<IRoomTypeDal>();
+            builder.RegisterType<EFStaffDal>().As<IStaffDal>();
+        }
+    }
+}
---
+++ YB.Business/DependicyInjection/AutoFac/InstanceFactory.cs
@@ -0,0 +1,23 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YB.Business.DependicyInjection.AutoFac
+{
+    public class InstanceFactory
+    {
+        public static T GetInstance<T>()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<AutofacBusinessModule>();
+            IContainer container = builder.Build();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                return scope.Resolve<T>();
+            }
+        }
+    }
+}
